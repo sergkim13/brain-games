7bb9ccffd17c18bd83cd54d5dde551c0049cfd22
Sergey Kim <sergkim7@gmail.com>
2022-09-29 17:06:14 +0300

    add welcome_user in cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+def greeter():
+    print('Welcome to the Brain Games!')
+
 
 def main():
-    print('Welcome to the Brain Games!')
+    greeter()
+    welcome_user()	
 
 if __name__ == '__main__':
     main()
