fb1687e5404dfb34337b133b725b7d3fda732401
Sergey Kim <sergkim7@gmail.com>
2022-10-01 16:30:18 +0300

    add even, brain_even

---
+++ brain_games/even.py
@@ -0,0 +1,29 @@
+import prompt
+from random import randint
+
+
+def even():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    correct_answer_counter = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while correct_answer_counter < 3:
+        random_number = randint(1, 100)
+        print(f'Question: {random_number}')
+        if random_number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct!')
+            correct_answer_counter += 1
+        else:
+            print(
+                f"'{answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name}!")
+            break
+    if correct_answer_counter == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games.even import even
+
+
+def greeter():
+    print('Welcome to the Brain Games!')
+
+
+def main():
+    greeter()
+    even()
+
+
+if __name__ == '__main__':
+    main()
